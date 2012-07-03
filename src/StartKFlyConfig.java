b0b98e738b205bd165685527113a3004a68e9cdd
Emil Fresk <ltu.korken@gmail.com>
2012-07-03 21:04:57 +0200

    Comments

--- src/StartKFlyConfig.java
+++ src/StartKFlyConfig.java
@@ -24,20 +24,26 @@ public class StartKFlyConfig {
 		System.out.println(wee);
 		
 		List<Byte> newList = new ArrayList<Byte>();
+		/* Header starts here */
 		newList.add((byte) 0xa6);
 		newList.add((byte) 0x01);
 		newList.add((byte) 0x02);
-		
-		newList.add((byte) 0xa6);
+		/* Header ends here - Data starts here */
+		newList.add((byte) 0xaa);
 		newList.add((byte) 0xbb);
+		/* Data ends here */
 		
 		for (byte b: MessageManipulation.MessagetoByteArray(MessageManipulation.CRCMessage(newList)))
 			System.out.print(IntToHex(b & 0xff) + " ");
 	}
 	
+	
+	/* *
+	 * This method makes all hex numbers in correct length.
+	 * */
 	public static String IntToHex(int value) {
 		String hex = Integer.toHexString(value);
-		hex = (hex.length() % 2 == 0) ? "" + hex : "0" + hex;
+		hex = (hex.length() % 2 == 0) ? hex : "0" + hex;
 		return hex;
 	}
 }
